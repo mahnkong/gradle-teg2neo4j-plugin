3ccfa8c77773f66d7eebedf0ee20092988d90c68
mahnkong <mahnkong@gmx.de>
2016-03-05 20:46:27 +0100

    tests & documentation

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+![Travis build status](https://api.travis-ci.org/mahnkong/gteg2neo4j.svg?branch=develop)
+
 # TODO: Add Content
+
+![Task Graph Visualization](./markdown-resources/graph.svg?raw=true)
--- src/main/groovy/mahnkong/gteg2neo4j/Gteg2Neo4jConstants.java
+++ src/main/groovy/mahnkong/gteg2neo4j/Gteg2Neo4jConstants.java
@@ -4,7 +4,7 @@ package mahnkong.gteg2neo4j;
  * Created by mahnkong on 28.02.2016.
  */
 public enum Gteg2Neo4jConstants {
-    EXTENSION_NAME("gteg2neo4j");
+    EXTENSION_NAME("gteg2neo4j"), DISABLE_GTEG2NEO2J_PROPERTY("mahnkong.gteg2neo4j.disabled");
 
     private String value;
     Gteg2Neo4jConstants(String value) {
